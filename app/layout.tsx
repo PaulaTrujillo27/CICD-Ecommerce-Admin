6c12f0b86209c8b90821f820defc848a3051cdf1
Paula Andrea Trujillo <71205932+PaulaTrujillo27@users.noreply.github.com>
2024-06-07 20:53:25 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Dashboard',
-  description: 'E-Commerce Dashboard New',
+  description: 'E-Commerce Dashboard Service',
 }
 
 export default async function RootLayout({
