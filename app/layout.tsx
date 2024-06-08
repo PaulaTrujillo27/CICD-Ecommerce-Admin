4668e0a6547bf43a2661cbc820af7c2f78144312
Paula Andrea Trujillo <71205932+PaulaTrujillo27@users.noreply.github.com>
2024-06-07 21:29:36 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Dashboard',
+  title: 'Dashboard Admin',
   description: 'E-Commerce Dashboard Service',
 }
 
